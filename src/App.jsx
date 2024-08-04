c3bfb226f5855bdd34a64e8840684fe01586c918
Balaji Dyavanpalli <balajidyavanpalli@gmail.com>
2024-08-04 20:47:56 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <i className="fas fa-spinner fa-spin fa-10x"></i>;
+
+  return (
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
+};
+
+export default App;
